52d2b30247953c4a734453e657be974b3151e086
Sasha <sashakozoriz2012@gmail.com>
2023-11-24 20:45:32 +0200

    React sort and filter example

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,63 @@
-import "./App.scss";
-import goods from "./goods.json";
-import { GoodList } from "./components/GoodList";
+import { useState } from 'react';
 
+import { GoodList } from './components/GoodList';
+import { Header } from './components/Header';
+import './App.scss';
+import goodsFromServer from './goods.json';
+import { SORT_FIELD } from './constants';
 
+function getPreparedGoods(goods, { sortField, query }) {
+  let preparedGoods = [...goods];
 
-const App = () => (
-  <div className="App">
-    <h1>Goods list</h1>
-    <GoodList goods={goods}/>
-  </div>
-);
+  if (query) {
+    preparedGoods = preparedGoods.filter((good) =>
+      good.name.toLowerCase().includes(query)
+    );
+  }
+
+  if (sortField) {
+    preparedGoods.sort((good1, good2) => {
+      switch (sortField) {
+        case SORT_FIELD.ID:
+          return good1[sortField] - good2[sortField];
+
+        case SORT_FIELD.NAME:
+        case SORT_FIELD.COLOR:
+          return good1[sortField].localeCompare(good2[sortField]);
+
+        default:
+          return 0;
+      }
+    });
+  }
+
+  return preparedGoods;
+}
+
+const App = () => {
+  const [sortField, setSortField] = useState('');
+  const [query, setQuery] = useState('');
+
+  const visibleGoods = getPreparedGoods(goodsFromServer, {
+    sortField,
+    query,
+  });
+  return (
+    <div className="App">
+      <Header
+        sortField={sortField}
+        sortBy={(field) => {
+          setSortField(field);
+        }}
+        query={query}
+        filterBy={(newQuery) => {
+          setQuery(newQuery);
+        }}
+      />
+
+      <GoodList goods={visibleGoods} />
+    </div>
+  );
+};
 
 export default App;
